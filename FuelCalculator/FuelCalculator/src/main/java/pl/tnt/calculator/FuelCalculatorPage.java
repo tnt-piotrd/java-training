5f7a2adcddbef78bc8a7a0ac4ef85f21ea58a1fb
Piotr Dubiela <piotrd.tnt@gmail.com>
2023-11-12 19:38:06 +0100

    waiting for page to be loaded

--- FuelCalculator/FuelCalculator/src/main/java/pl/tnt/calculator/FuelCalculatorPage.java
+++ FuelCalculator/FuelCalculator/src/main/java/pl/tnt/calculator/FuelCalculatorPage.java
@@ -1,10 +1,15 @@
 package pl.tnt.calculator;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class FuelCalculatorPage {
     @FindBy(name = "tripdistance")
@@ -36,7 +41,13 @@ public class FuelCalculatorPage {
 
     public FuelCalculatorPage(WebDriver driver) {
         this.driver = driver;
+        waitForPageToBeLoaded();
         PageFactory.initElements(driver, this);
+    }
+
+    public void waitForPageToBeLoaded(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='button'][@value='Clear']")));
     }
 
     public String calculateFuelAndCost(String distance, boolean isDistanceInKilometers, String fuelEfficiency, String gasPrice, boolean isPricePerLiter){
